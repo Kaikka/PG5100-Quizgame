4e2af2acc92c8bafafb09ab1c90888570b422c7b
Kaikka <kaileeto@gmail.com>
2021-02-19 17:52:12 +0100

    ex05 done - resetEbj semi done myself i guess. No null check though

--- quizgame/src/test/java/org/quizgame/ejb/ResetEjb.java
+++ quizgame/src/test/java/org/quizgame/ejb/ResetEjb.java
@@ -16,33 +16,14 @@ public class ResetEjb {
     @PersistenceContext
     private EntityManager em;
 
-/*    public void deleteDb() {
-        em.clear();
-    }*/
-
     public void deleteDb(){
         deleteEntities(Quiz.class);
         deleteEntities(SubCategory.class);
         deleteEntities(Category.class);
     }
 
-    private void deleteEntities(Class<?> entity){
-
-        if(entity == null || entity.getAnnotation(Entity.class) == null){
-            throw new IllegalArgumentException("Invalid non-entity class");
-        }
-
-        String name = entity.getSimpleName();
-
-        /*
-            Note: we passed as input a Class<?> instead of a String to
-            avoid SQL injection. However, being here just test code, it should
-            not be a problem. But, as a good habit, always be paranoiac about
-            security, above all when you have code that can delete the whole
-            database...
-         */
-
-        Query query = em.createQuery("delete from " + name);
-        query.executeUpdate();
+    private void deleteEntities(Class<?> entity) {
+        Query q = em.createQuery("DELETE FROM " + entity.getSimpleName());
+        q.executeUpdate();
     }
 }
